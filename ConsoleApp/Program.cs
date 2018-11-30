b9a67bd58c3f4d84f45536c3ae7d6247e8cafafe
Steve Land <steveland83@gmail.com>
2018-11-30 09:58:56 +0000

    feat/ async void main console example

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var someClass = new SomeClass();
+            await someClass.OutputSomethingAsync();
+
+            Console.WriteLine("\nPress any key to exit.");
+            Console.ReadKey();
+        }
+
+
+        public class SomeClass
+        {
+            public async Task OutputSomethingAsync()
+            {
+                await Task.Delay(500);
+                Console.WriteLine("\nThis is from an async method...");
+            }
+        }
+    }
+}
